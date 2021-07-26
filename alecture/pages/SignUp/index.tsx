08dab28edf72ec7ee1017e3f94482e0b04b11600
문석영(Seokyoung Moon)/Biz플랫폼개발팀/SKP <seokyoung.moon90@gmail.com>
2021-07-26 23:48:56 +0900

    ✨ 회원가입 페이지 만들기

--- alecture/pages/SignUp/index.tsx
+++ alecture/pages/SignUp/index.tsx
@@ -1,7 +1,113 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import {
+  Form,
+  Label,
+  Input,
+  LinkContainer,
+  Button,
+  Header,
+  Error,
+} from './style';
 
 const SignUp = () => {
-  return <div>회원가입</div>;
+  const [email, setEmail] = useState('');
+  const [nickname, setNickname] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordCheck, setPasswordCheck] = useState('');
+  const [mismatchError, setMismatchError] = useState(false);
+
+  // 성능최적화를 위해 useCallback 사용 -> []에 추가한 state가 변경되면 함수를 재실행함(리랜더링을 줄임)
+  const onChangeEmail = useCallback((e) => {
+    setEmail(e.target.value);
+  }, []);
+  const onChangeNickname = useCallback((e) => {
+    setNickname(e.target.value);
+  }, []);
+  const onChangePassword = useCallback(
+    (e) => {
+      setPassword(e.target.value);
+      setMismatchError(e.target.value !== passwordCheck);
+    },
+    [passwordCheck] // 함수 외부 것만 추가하면됨
+  );
+  const onChangePasswordCheck = useCallback(
+    (e) => {
+      setPasswordCheck(e.target.value);
+      setMismatchError(e.target.value !== password);
+    },
+    [password]
+  );
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (!mismatchError) {
+        console.log('가보자고');
+      }
+    },
+    [email, nickname, password, passwordCheck]
+  );
+
+  return (
+    <div id="container">
+      <Header>Sleact</Header>
+      <Form onSubmit={onSubmit}>
+        <Label id="email-label">
+          <span>이메일 주소</span>
+          <div>
+            <Input
+              type="email"
+              id="email"
+              name="email"
+              value={email}
+              onChange={onChangeEmail}
+            />
+          </div>
+        </Label>
+        <Label id="nickname-label">
+          <span>닉네임</span>
+          <div>
+            <Input
+              type="text"
+              id="nickname"
+              name="nickname"
+              value={nickname}
+              onChange={onChangeNickname}
+            />
+          </div>
+        </Label>
+        <Label id="password-label">
+          <span>비밀번호</span>
+          <div>
+            <Input
+              type="password"
+              id="password"
+              name="password"
+              value={password}
+              onChange={onChangePassword}
+            />
+          </div>
+        </Label>
+        <Label id="password-check-label">
+          <span>비밀번호 확인</span>
+          <div>
+            <Input
+              type="password"
+              id="password-check"
+              name="password-check"
+              value={passwordCheck}
+              onChange={onChangePasswordCheck}
+            />
+          </div>
+          {mismatchError && <Error>비밀번호가 일치하지 않습니다.</Error>}
+        </Label>
+        <Button type="submit">회원가입</Button>
+      </Form>
+      <LinkContainer>
+        이미 회원이신가요?&nbsp;
+        <a href="/login">로그인 하러가기</a>
+      </LinkContainer>
+    </div>
+  );
 };
 
 export default SignUp;
