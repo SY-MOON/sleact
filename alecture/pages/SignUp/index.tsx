93d7b2961598c0b8d302e06a7fcb9befbb67ed05
문석영(Seokyoung Moon)/Biz플랫폼개발팀/SKP <seokyoung.moon90@gmail.com>
2021-07-29 22:35:25 +0900

    ✨ 로그인 페이지 만들기

--- alecture/pages/SignUp/index.tsx
+++ alecture/pages/SignUp/index.tsx
@@ -1,6 +1,7 @@
 import useInput from '@hooks/useInput';
 import axios from 'axios';
 import React, { useCallback, useState } from 'react';
+import { Link } from 'react-router-dom';
 import {
   Form,
   Label,
@@ -125,7 +126,7 @@ const SignUp = () => {
       </Form>
       <LinkContainer>
         이미 회원이신가요?&nbsp;
-        <a href="/login">로그인 하러가기</a>
+        <Link to="/login">로그인 하러가기</Link>
       </LinkContainer>
     </div>
   );
