0e407b26bb74c0d3dc745437e04966bea51081a1
문석영(Seokyoung Moon)/Biz플랫폼개발팀/SKP <seokyoung.moon90@gmail.com>
2021-08-01 21:21:06 +0900

    ✨ 메뉴 만들기

--- alecture/layouts/workspace/index.tsx
+++ alecture/layouts/workspace/index.tsx
@@ -2,24 +2,28 @@ import {
   Channels,
   Chats,
   Header,
+  LogOutButton,
   MenuScroll,
   ProfileImg,
+  ProfileModal,
   RightMenu,
   WorkspaceName,
   WorkspaceWrapper,
 } from '@layouts/workspace/style';
 import fetcher from '@utils/fetcher';
 import axios from 'axios';
-import React, { VFC, useCallback } from 'react';
+import React, { VFC, useCallback, useState } from 'react';
 import { Redirect, Route, Switch } from 'react-router';
 import useSWR from 'swr';
 import gravatar from 'gravatar';
 import loadable from '@loadable/component';
+import Menu from '@components/Menu';
 
 const Channel = loadable(() => import('@pages/Channel'));
 const DirectMessage = loadable(() => import('@pages/DirectMessage'));
 
 const Workspace: VFC = () => {
+  const [showUserMenu, setShowUserMenu] = useState(false);
   const { data, error, revalidate, mutate } = useSWR(
     'http://localhost:3095/api/users',
     fetcher,
@@ -35,6 +39,10 @@ const Workspace: VFC = () => {
       });
   }, []);
 
+  const onClickUserProfile = useCallback(() => {
+    setShowUserMenu((prev) => !prev);
+  }, []);
+
   if (data === false) {
     return <Redirect to="/login" />;
   }
@@ -42,14 +50,35 @@ const Workspace: VFC = () => {
     <div>
       <Header>
         <RightMenu>
-          <span>
+          <span onClick={onClickUserProfile}>
             <ProfileImg
               src={gravatar.url(data?.nickname, { s: '28px', d: 'retro' })}
             />
+            {showUserMenu && (
+              <Menu
+                style={{ right: 0, top: 38 }}
+                show={showUserMenu}
+                onCloseModal={onClickUserProfile}
+              >
+                <ProfileModal>
+                  <img
+                    src={gravatar.url(data?.nickname, {
+                      s: '36px',
+                      d: 'retro',
+                    })}
+                  />
+                  <div>
+                    <span id="profile-name">{data?.nickname}</span>
+                    <span id="profile-active">Active</span>
+                  </div>
+                </ProfileModal>
+                <LogOutButton onClick={onLogout}>로그아웃</LogOutButton>
+              </Menu>
+            )}
           </span>
         </RightMenu>
       </Header>
-      <button onClick={onLogout}>로그아웃</button>
+
       <WorkspaceWrapper>
         <Channels>
           <WorkspaceName>Sleact</WorkspaceName>
